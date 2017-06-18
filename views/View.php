33670a330350be69b83f8e8a644eb2102f6a4322
user <brewbaker@yandex.com>
2017-06-18 13:04:51 +0300

    Create View.php

---
+++ views/View.php
@@ -0,0 +1,13 @@
+<?php
+	class View {
+		function __construct() {
+			echo '<!--This is the master view<br />-->';
+		}
+
+		public function render($name) {
+			require 'views/header.php';
+			require 'views/' . $name;
+			require 'views/footer.php';
+		}
+	}
+?>
