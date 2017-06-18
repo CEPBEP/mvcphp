14a3d3c5440a79ee0778d08b9807087a69489501
user <brewbaker@yandex.com>
2017-06-18 13:03:53 +0300

    Create footer.php

---
+++ views/footer.php
@@ -0,0 +1,9 @@
+	<!--</div>
+  <div class="l-page__footer">
+<button type="button" class="btn btn-warning">  <a href='tel:89859351863' style=''><i class='fa fa-phone fa-3x' aria-hidden='true'></i></a></button>
+<button type="button" class="btn btn-warning">  <a href='tel:89859351863' style=''><i class='fa fa-users fa-3x' aria-hidden='true'></i></a></button>
+<button type="button" class="btn btn-warning">  <a href='tel:89859351863' style=''><i class='fa fa-user fa-3x' aria-hidden='true'></i></a></button>
+<button type="button" class="btn btn-warning">  <a href='tel:89859351863' style=''><i class='fa fa-phone fa-3x' aria-hidden='true'></i></a></button>
+</div>-->
+</body>
+</html>
