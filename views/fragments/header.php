03c4f58eaae346092291184bee53745fd12dac22
user <brewbaker@yandex.com>
2017-06-18 13:01:32 +0300

    Create header.php

---
+++ views/fragments/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div id='header'>
+		<h1>THIS IS THE HEADER FRAGMENT</h1>
+	</div>
+
+	<div id="content">
+		<!-- Content supplied by PHP goes here -->
