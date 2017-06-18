b0bed4b8c371aacac7064613850503f38986e733
user <brewbaker@yandex.com>
2017-06-18 12:44:12 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+	// Use an autoloader
+	require 'libs/Bootstrap.php';
+	require 'models/Model.php';
+	require 'views/View.php';
+	require 'controllers/Controller.php';
+	$app = new Bootstrap();
+?>
