94e8cde11a39372a1a84c9226929ffc96785fd45
user <brewbaker@yandex.com>
2017-06-18 13:04:23 +0300

    Create header.php

---
+++ views/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+  <title>Uber Like App</title>
+  <script src="//use.fontawesome.com/fe69f16b44.js"></script>
+  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="//ruservis.github.io/css/style.css">
+</head>
+<body>
+ <!-- <div class="l-page">
+   <div class="l-main">
+    <div class="l-main__header"><button type="button" class="btn btn-warning"><a href='#' style=''><i class='fa fa-globe fa-3x' aria-hidden='true'></i></a></button>
+ <button type="button" class="btn btn-warning"> <a href='tel:89859351863' style=''><i class='fa fa-map fa-3x' aria-hidden='true'></i></a></button>
+ <button type="button" class="btn btn-warning"> <a href='tel:89859351863' style=''><i class='fa fa-street-view fa-3x' aria-hidden='true'></i></a></button>
+ <button type="button" class="btn btn-warning"> <a href='tel:89859351863' style=''><i class='fa fa-map-marker fa-3x' aria-hidden='true'></i></a></button>
+ <button type="button" class="btn btn-warning"> <a href='tel:89859351863' style=''><i class='fa fa-location-arrow fa-3x' aria-hidden='true'></i></a></button></div>-->
